2b32c0db1aa90362640a4cd3d9203585d7d1a732
Bleb1k <viktor.masnikov17283940@gmail.com>
2023-10-18 00:51:55 +0900

    task not well suited for the subject

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+use std::{borrow::Cow, env, path::PathBuf};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long, value_hint = clap::ValueHint::DirPath)]
+    conf: Option<String>,
+}
+
+struct LazyPath<'a> {
+    conf: Cow<'a, PathBuf>,
+}
+
+impl<'a> LazyPath<'a> {
+    fn detect() -> Self {
+        Self {
+            conf: if let Some(path) = Args::parse().conf {
+                Cow::Owned(path.into())
+            } else if let Ok(path) = env::var("APP_CONF") {
+                Cow::Owned(path.into())
+            } else {
+                Cow::Owned("/etc/app/app.conf".into())
+            },
+        }
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let a: LazyPath = LazyPath::detect();
+    println!("{}", a.conf.display());
 }
